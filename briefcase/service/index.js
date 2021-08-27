38fd3013d8a26d1582fdb380c3a17b2ec3334e08
soumya.aithal <soumya.aithal@workday.com>
2021-08-27 09:42:53 -0700

    add ip address

--- briefcase/service/index.js
+++ briefcase/service/index.js
@@ -1,13 +1,14 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 8080;                  //Save the port number where your server will be listening
-const https = require('https');
+const http = require('http');
 
 //Idiomatic expression in express to route and respond to a client request
 
 app.get('/callback', async function (req, res) {
-    //let unlocked = https.get("http://a.b.c.d/unloc?${req.query.code}");
-    console.log("callback called")
+
+    let unlocked = http.get("http:// 192.168.86.39/unlock");
+    console.log("callback called ", unlocked)
     return res.status(200).send("hello")
 })
 
